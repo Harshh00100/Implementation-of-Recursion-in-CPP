b2f86a75a08f136a74aef7014a9f943685bfdb66
Harsh Kumar <kunarharsh76@gmail.com>
2025-09-19 20:43:36 +0530

    Add files via upload

---
+++ EXP15-d.cpp
@@ -0,0 +1,27 @@
+//Harsh Kumar
+//24070123146
+//B3
+#include <iostream>
+#include <string.h>
+
+void print_rev(int i){
+    if(i>0) 
+    {
+        std::cout<<(i%10);
+        print_rev(i/10); 
+    }
+}
+    
+int main(){
+    int i;
+    std::cout<<"Enter the number: ";
+    std::cin>>i;
+    std::cout<<"The reverse of the number is: ";
+    print_rev(i); 
+    return 0;
+}
+
+/*Output:
+Enter the number: 1234567890
+The reverse of the number is: 0987654321
+*/
